564fafc8f2d5135e5598b54db61714b2bbde4824
Virginie Trubiano & Conor Baumgart <student@epicodus.com>
2015-03-24 09:54:57 -0700

    Add initial files and begin app.php

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once __DIR__."/../vendor/autoload.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $DB = new PDO('pgsql:host=localhost;dbname=registrar');
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array ('twig.path' => __DIR__."/../views"));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.twig');
+    });
+
+
+
+
+
+
+
+?>
