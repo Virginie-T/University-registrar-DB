581795f41c45bb274221a071ab0b8c403b20e6d7
Virginie Trubiano & Conor Baumgart <student@epicodus.com>
2015-03-24 10:31:24 -0700

    Add private, contruct, getters and setters for new class Student

---
+++ src/Student.php
@@ -0,0 +1,45 @@
+<?php
+
+    class Register
+    {
+        private $name;
+        private $date;
+        private $id;
+
+        function __construct($name_student, $date_enrolled, $id = null)
+        {
+            $this->name = $name_student;
+            $this->date = $date_enrolled;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->$name = (string) $new_name;
+        }
+
+        function getDate()
+        {
+            return $this->date;
+        }
+
+        function setDate($new_date)
+        {
+            $this->date = (string) $new_date;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = (int) $new_id;
+        }
+    }
